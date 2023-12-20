0c6e2bd3875b4a20518826cd28e0eb7128a1c340
Max P <Mail@MPassarello.de>
2023-12-20 14:09:34 +0100

    Function added, which allows you to individualize the footnotes and thus make them unique.
    The function also corrects the footnote numbering.
    
    English comments added.

--- EventfulContentFilter.php
+++ EventfulContentFilter.php
@@ -44,9 +44,58 @@ class EventfulContentFilter extends AbstractPicoPlugin
                 $pageData['content'] = $pico->parseFileContent($markdown);
                 $pico->triggerEvent('onContentParsed', array(&$pageData['content']));
             }
-
+            $pageData['content'] = $this->individualizeFootnotes($pageData['content'], 'u' . str_replace("/", "", $pageId));
             return $pageData['content'];
         }
         return null;
     }
+
+    private function individualizeFootnotes($html, $stringToAdd)
+    {
+        // Create a DOMDocument and load the HTML code
+        $dom = new DOMDocument();
+        @$dom->loadHTML(mb_convert_encoding($html, 'HTML-ENTITIES', 'UTF-8'), LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+
+        // Individualize the sup elements and their children
+        $supElements = $dom->getElementsByTagName('sup');
+        foreach ($supElements as $sup) {
+            if ($sup->hasAttribute('id')) {
+                $sup->setAttribute('id', $sup->getAttribute('id') . $stringToAdd);
+            }
+            foreach ($sup->getElementsByTagName('a') as $a) {
+                if ($a->hasAttribute('href')) {
+                    // Extract the number from the href attribute
+                    $href = $a->getAttribute('href');
+                    if (preg_match('/#fn:(\d+)/', $href, $matches)) {
+                        $number = $matches[1];
+                        // Replace the text in the a tag with the extracted number
+                        $a->nodeValue = $number;
+                    }
+                    $a->setAttribute('href', $href . $stringToAdd);
+                }
+            }
+        }
+
+        // Individualize the li elements in the footnotes div
+        $divs = $dom->getElementsByTagName('div');
+        foreach ($divs as $div) {
+            if ($div->getAttribute('class') == 'footnotes') {
+                $lis = $div->getElementsByTagName('li');
+                foreach ($lis as $li) {
+                    if ($li->hasAttribute('id')) {
+                        $li->setAttribute('id', $li->getAttribute('id') . $stringToAdd);
+                    }
+                    foreach ($li->getElementsByTagName('a') as $a) {
+                        if ($a->getAttribute('class') == 'footnote-backref' && $a->hasAttribute('href')) {
+                            $a->setAttribute('href', $a->getAttribute('href') . $stringToAdd);
+                        }
+                    }
+                }
+            }
+        }
+
+        // Return the modified HTML code
+        return $dom->saveHTML();
+    }
+
 }
